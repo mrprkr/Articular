8bd9513a7ff267e51c62753a3cc3d4e0c73ef2d1
Michael Parker <parker_michael@me.com>
2015-01-28 16:23:28 +1100

    added design pages

--- src/js/min/app-min.js
+++ src/js/min/app-min.js
@@ -1,8 +1,8 @@
 var angular = angular;
 
-var app = angular.module("app", [ "ngRoute" ]);
+var app = angular.module("app", [ "ngRoute", "ng-contentful" ]);
 
-app.config(function($routeProvider) {
+app.config(function($routeProvider, contentfulClientProvider) {
     $routeProvider.when("/", {
         templateUrl: "src/html/home.html",
         controller: "mainController"
@@ -16,14 +16,22 @@ app.config(function($routeProvider) {
         templateUrl: "src/html/design.html",
         controller: "designController"
     });
+    contentfulClientProvider.setSpaceId("qj4662rfubip");
+    contentfulClientProvider.setAccessToken("c35b82ea8c5dad62950fa76f7a3c05459a8c4166b4d1ecdf7e52048757d6a50c");
 });
 
-app.controller("mainController", function($scope) {
+app.controller("mainController", function($scope, contentfulClient) {
+    contentfulClient.space().then(function(space) {
+        $scope.space = space;
+    });
+    contentfulClient.entries().then(function(entries) {
+        $scope.entries = entries;
+    });
     $scope.test = "angular actually works!";
 });
 
 app.controller("designController", function($scope) {
-    $scope.sidebarItems = [ "Global", "Home", "Events", "News" ];
+    $scope.test = "test";
 });
 
 app.controller("documentationController", function($scope) {
